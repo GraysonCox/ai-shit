3f59f82bf823e6b61b28c587b0454406ac795a10
Grayson Cox <35706281+GraysonCox@users.noreply.github.com>
2026-08-08 14:55:31 -0500

    Add Discord bot (#2)
    * Eh?
    
    * Eh???
    
    * Please
    
    * Eh?
    
    * Eh?

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-pass
+import logging
+import os
+
+from .controllers import discord_bot
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    discord_token = os.getenv("DISCORD_TOKEN")
+    if not discord_token:
+        raise ValueError("DISCORD_TOKEN is a required environment variable.")
+    discord_bot.run(discord_token)
+
+
+if __name__ == "__main__":
+    main()
